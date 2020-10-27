82ac182b135c0100256a475fd7bc2d53071c69bd
Yichen Xu <yichen.x@outlook.com>
2020-10-27 16:24:45 +0800

    Update cabal profile

--- katas.nix
+++ katas.nix
@@ -1,10 +1,14 @@
-{ mkDerivation, base, containers, lens, mtl, random, stdenv }:
+{ mkDerivation, base, containers, lens, megaparsec, mtl, random
+, stdenv, text
+}:
 mkDerivation {
   pname = "katas";
   version = "1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base containers lens mtl random ];
+  executableHaskellDepends = [
+    base containers lens megaparsec mtl random text
+  ];
   license = stdenv.lib.licenses.bsd3;
 }
