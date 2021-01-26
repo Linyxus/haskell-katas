b61fa0ef320a3117aaea41272f95ced34e47afcb
Yichen Xu <yichen.x@outlook.com>
2021-01-26 11:39:37 +0800

    Add random into dependency

--- katas.nix
+++ katas.nix
@@ -1,10 +1,10 @@
-{ mkDerivation, base, containers, lens, lib, mtl }:
+{ mkDerivation, base, containers, lens, lib, mtl, random }:
 mkDerivation {
   pname = "katas";
   version = "1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base containers lens mtl ];
+  executableHaskellDepends = [ base containers lens mtl random ];
   license = lib.licenses.bsd3;
 }
