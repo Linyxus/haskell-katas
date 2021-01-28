681f634d79d279718a45cd12cfb0bab67dc8f33c
Yichen Xu <yichen.x@outlook.com>
2021-01-28 17:03:10 +0800

    Add auto-generated nix shell file

--- shell.nix
+++ shell.nix
@@ -1,1 +1,28 @@
-(import ./default.nix).project.env
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, containers, lens, lib, mtl, random }:
+      mkDerivation {
+        pname = "katas";
+        version = "1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [ base containers lens mtl random ];
+        license = lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
